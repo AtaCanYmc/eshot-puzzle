30db3eb12068016dd1aa6ad5e8df9bb211c80053
Ata Can <atacanymc@gmail.com>
2026-04-20 01:10:19 +0300

    feat: update sidebar toggle icon to show up/down arrow based on open state

--- src/components/sidebar/MobileSideBar.tsx
+++ src/components/sidebar/MobileSideBar.tsx
@@ -228,8 +228,13 @@ const MobileSideBar: React.FC<MobileSideBarProps> = (props: MobileSideBarProps) 
             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
-                    <line x1="18" y1="6" x2="6" y2="18"/>
-                    <line x1="6" y1="6" x2="18" y2="18"/>
+                  {isSidebarOpen ? (
+                    // Aşağı ok
+                    <polyline points="6 9 12 15 18 9" />
+                  ) : (
+                    // Yukarı ok
+                    <polyline points="6 15 12 9 18 15" />
+                  )}
                 </svg>
             </button>
         </aside>
